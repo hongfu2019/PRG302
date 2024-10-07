06969fb181a96764c4bc10e9a5c1e8742f8c7077
Hong <52814111+hongfu2019@users.noreply.github.com>
2024-10-07 10:58:24 +0800

    Create doctest01.py

---
+++ doctest01.py
@@ -0,0 +1,24 @@
+import argparse
+import doctest
+
+def add(a, b):
+    """
+    Returns the sum of a and b.
+
+    >>> add(2, 3)
+    5
+    >>> add(-1, 1)
+    0
+    """
+    return a + b
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run the program or doctests.")
+    parser.add_argument('--doctest', action='store_true', help="Run doctests")
+    args = parser.parse_args()
+
+    if args.doctest:
+        doctest.testmod(verbose=True)
+    else:
+        # Normal program execution (replace with actual functionality)
+        print("Run the program normally.")
